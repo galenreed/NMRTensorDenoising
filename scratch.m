261ba15a5a2a50658bd6fd67f00a9b0138124a4c
Reed <212669748@logon.ds.ge.com>
2017-10-19 16:36:13 -0500

    options for magnitude or phase sensitive processing

--- scratch.m
+++ scratch.m
@@ -4,6 +4,10 @@ addpath('utils');
 addpath('utils/matNMR');
 addpath('utils/tensorlab');
 
+phaseSensitive = false;
+% set to false for applying same phase correction to all timepoints from a single coil
+phaseCorrectTimePoints = true;
+
 infile = 'spectra1.mat';
 load(infile);
 dataSize = size(data);
@@ -11,27 +15,49 @@ specPoints = dataSize(1);
 timePoints = dataSize(2);
 channels = dataSize(3);
 
-% run through all the matrix and do FFTs
+disp("computing FFTs");
 specs = zeros(size(data));
 for ii = 1:channels
   for jj = 1:timePoints
     fid = squeeze(data(:,jj,ii));
     spec = fftshift(fft(fftshift(fid)));
-    specs(:,jj,ii) = (spec);    
+    specs(:,jj,ii) = spec; 
+  end
+end
+
+phaseCorrectedSpectra =  zeros(size(specs));
+if(~phaseSensitive)
+  phaseCorrectedSpectra = abs(specs);% makes life a lot easier
+else 
+  disp("performing coil-wise phase correction");
+
+  % here we take the highest signal spec from a single channel, estimate its phase
+  % then apply that phase to all time points from that channel
+  for ii = 1:channels        
+    TFSpecSingleCoil = specs(:, :, ii);
+    if(~phaseCorrectTimePoints)
+      peakTimePoint = find(max(max(abs(TFSpecSingleCoil))));% 
+      highestSNRSpec = TFSpecSingleCoil(:, peakTimePoint);
+      phi0 = phaseCorrectSpectra(highestSNRSpec);
+      TFSpecSingleCoil = TFSpecSingleCoil * exp(-1i * pi * phi0 / 180);
+      phaseCorrectedSpectra(:, :, ii) = TFSpecSingleCoil;
+    else
+      % just run through all time points 
+      for jj = 1:timePoints
+        singleSpectra = TFSpecSingleCoil(:, jj);
+        phi0 = phaseCorrectSpectra(singleSpectra);
+        phaseCorrectedSpectra(:, jj, ii) =  singleSpectra * exp(-1i * pi * phi0 / 180);
+      end     
+    end
   end
 end
 
 
-testspec = specs(:,1,1);
-phi0 = phaseCorrectSpectra(testspec);
-phased = testspec * exp(-1i * phi0 * pi / 180);
-plot(real(phased))
 
 
 
 
-#{
-[U,S,sv] = mlsvd(specs);
+[U,S,sv] = mlsvd(phaseCorrectedSpectra);
 
 figure();
 for ii = 1:3
@@ -51,7 +77,7 @@ specsLR = lmlragen(Utrunc, Strunc);
 
 denoised = sum(specsLR,3);
 denoised = sum(denoised,2);
-regular = sum(specs,3);
+regular = sum(phaseCorrectedSpectra,3);
 regular = sum(regular,2);
 
 figure();
@@ -59,6 +85,6 @@ subplot(1,2,1)
 plot(real(regular))
 subplot(1,2,2)
 plot(real(denoised))
-#}
+
 
 
