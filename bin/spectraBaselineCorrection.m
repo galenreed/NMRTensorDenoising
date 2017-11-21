312759fa3ce0974f6a175b4cbb294bf818907a95
Reed <212669748@logon.ds.ge.com>
2017-11-21 12:33:31 -0600

    baseline correction added as git submodule

--- bin/spectraBaselineCorrection.m
+++
@@ -1,7 +0,0 @@
-function correctedspectrum = spectraBaselineCorrection(spectrum)
-  
-  mask = peakDetect(spectrum);
-  baseline = estimateBaseline(spectrum, mask);
-  correctedspectrum = spectrum - baseline;
-  
-end
